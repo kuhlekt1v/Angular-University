b283af566d20730000591de5ad271f0d605bd10f
Cody Sheridan <cody.sheridan@protonmail.com>
2024-02-10 07:04:17 -0500

    in progress

--- angular-dd/src/app/app.component.ts
+++ angular-dd/src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { AfterViewInit, Component } from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  courses: Course[] = COURSES;
 
-
-
+  constructor() {}
+  ngAfterViewInit(): void {}
+  onCourseSelected(course: Course) {}
 }
--- angular-dd/src/app/course-card/course-card.component.ts
+++ angular-dd/src/app/course-card/course-card.component.ts
@@ -1,8 +1,34 @@
-import { Component } from "@angular/core";
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ContentChild,
+} from "@angular/core";
+import { Course } from "../model/course";
+import { CourseImageComponent } from "../course-image/course-image.component";
+import { CommonModule } from "@angular/common";
 @Component({
   selector: "course-card",
   templateUrl: "./course-card.component.html",
   styleUrl: "./course-card.component.css",
   standalone: true,
+  imports: [CommonModule],
 })
-export class CourseCardComponent {}
+export class CourseCardComponent {
+  @Input({ required: true })
+  course: Course;
+
+  @Input()
+  cardIndex: number;
+
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+  @ContentChild(CourseImageComponent)
+  image: CourseImageComponent;
+
+  onCourseView() {
+    this.courseSelected.emit(this.course);
+  }
+}
